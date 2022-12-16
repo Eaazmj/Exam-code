dd3544be8bba08f04946776aadcd359450a7cc1e
Eaazmj <113166037+Eaazmj@users.noreply.github.com>
2022-12-16 12:06:20 +0100

    16.12 12:06

---
+++ js/js.js
@@ -0,0 +1,8 @@
+function changeImage(){
+    yyy.src = "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/059/083/original/cfg3.png?1671151658"
+}
+
+function changeImage(){
+    xxx.src = "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/059/083/original/cfg3.png?1671151658"
+    yyy.innerHTML = "start working"
+}
