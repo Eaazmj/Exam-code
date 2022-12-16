a85e82e98a711d839bfa4b15c0912eed38f42812
Eaazmj <113166037+Eaazmj@users.noreply.github.com>
2022-12-16 12:48:54 +0100

    16.12 12:48

--- js/js.js
+++ js/js.js
@@ -1,8 +1,7 @@
-function changeImage(){
-    yyy.src = "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/059/083/original/cfg3.png?1671151658"
-}
 
 function changeImage(){
     xxx.src = "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/059/083/original/cfg3.png?1671151658"
-    yyy.innerHTML = "start working"
+    yyy.style.fontSize = "2rem"
+    zzz.style.fontSize = "smaller"
 }
+
