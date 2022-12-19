d773004b1b54c0f14e6ce5dab4a09a015078785d
Eaazmj <113166037+Eaazmj@users.noreply.github.com>
2022-12-19 17:58:46 +0100

    19.12

--- js/js.js
+++ js/js.js
@@ -1,7 +1,25 @@
 
-function changeImage(){
-    xxx.src = "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/059/083/original/cfg3.png?1671151658"
-    yyy.style.fontSize = "2rem"
-    zzz.style.fontSize = "smaller"
-}
+      const contentItems = document.querySelectorAll(".content-item");
+      const navigationItems = document.querySelectorAll(".navigation-item");
+
+      let activeIndex = 0;
+      function switchContent(indexToBeActivated) {
+        contentItems[activeIndex].removeAttribute("data-is-active");
+        contentItems[indexToBeActivated].setAttribute("data-is-active", "");
+
+        /* also add the attribute to the navigation item */
+        navigationItems[activeIndex].removeAttribute("data-is-active");
+        navigationItems[indexToBeActivated].setAttribute("data-is-active", "");
+
+        activeIndex = indexToBeActivated;
+      }
+
+      switchContent(0);
+
+      navigationItems.forEach((navigationItem, index) => {
+        navigationItem.addEventListener("click", () => {
+          switchContent(index);
+        });
+      });
+
 
